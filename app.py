e7ecb6cb3b0f4c6b70de042f88d75acd8e1c36c3
Ken Klabnik <ken.klabnik@gmail.com>
2024-09-10 13:53:55 -0400

    correct imlementation of outlier-hiding checkbox

--- app.py
+++ app.py
@@ -4,6 +4,26 @@ import plotly.express as px
 
 df = pd.read_csv('vehicles_us.csv')
 
+#version of the chart with outlier prices removed
+df_cut = df[(df['price'] < 100000)].copy() #avoid SettingWithCopy warning later
+
+#generate charts
+
+outlier_disclaimer = ' (priced under $100k)'
+
+fig_hist = px.histogram(data_frame=df['price'], nbins=200,
+             width=900, title='Price Distribution' + outlier_disclaimer,
+             labels={
+                 'value': 'Price (USD)',
+                 'count': 'Number of cars'
+             })
+
+fig_year = px.scatter(data_frame=df, y='price', x='model_year', title='Price vs. Model Year' + outlier_disclaimer, opacity=0.3)
+
+order_list = ['new', 'like new', 'excellent', 'good', 'fair', 'salvage']
+fig_cond = px.scatter(data_frame=df, y='price', x='condition', title='Price vs. Condition' + outlier_disclaimer, opacity=0.3,
+                 category_orders={'condition': order_list})
+
 #title
 st.title('VehicleData')
 st.text('A web development demo powered by Streamlit')
@@ -16,42 +36,20 @@ st.dataframe(df)
 
 st.title('Options')
 
-#for handling the checkbox
-hiding_outliers=True
+hide_outliers = st.checkbox('Hide Cars Above $100,000 Asking Price', value=False)
 
-df_cut = df[(df['price'] < 100000)].copy() #avoid SettingWithCopy warning later
-df_used = df_cut
-outlier_disclaimer = ' (priced under $100k)'
+if hide_outliers:
+    fig_hist.data[0].update(x=df_cut['price'])
+    fig_year.data[0].update(x=df_cut['model_year'], y=df_cut['price'])
+    fig_cond.data[0].update(x=df_cut['condition'], y=df_cut['price'])
 
-def handle_outliers():
-    if hiding_outliers:
-        hiding_outliers = False
-        outlier_disclaimer = ''
-        df_used = df
-    else:
-        hiding_outliers = True
-        outlier_disclaimer = ' (priced under $100k)'
-        df_used = df_cut
-
-st.checkbox('Hide Cars Above $100,000 Asking Price', value=hiding_outliers, on_change=handle_outliers)
 
 #Price histogram
 st.header('Price Distribution')
-fig1 = px.histogram(data_frame=df['price'], nbins=200, range_x=[0,100000],
-             width=900, title='Price Distribution (All Cars Under $100k)',
-             labels={
-                 'value': 'Price (USD)',
-                 'count': 'Number of cars'
-             })
-st.plotly_chart(fig1)
+st.plotly_chart(fig_hist)
 
 st.title('Price vs. Model Year scatter plot')
-fig2 = px.scatter(data_frame=df_used, y='price', x='model_year', title='Price vs. Model Year' + outlier_disclaimer, opacity=0.3)
-st.plotly_chart(fig2)
+st.plotly_chart(fig_year)
 
 st.title('Price vs Condition scatter plot')
-order_list = ['new', 'like new', 'excellent', 'good', 'fair', 'salvage']
-
-fig3 = px.scatter(data_frame=df_cut, y='price', x='condition', title='Price vs. Condition' + outlier_disclaimer, opacity=0.3,
-                 category_orders={'condition': order_list})
-st.plotly_chart(fig3)
+st.plotly_chart(fig_cond)
